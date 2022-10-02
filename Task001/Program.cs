8f07f52309e4056e29d407867142ae07189f93ff
Akmaral <axma.mail@gmail.com>
2022-10-03 01:23:07 +0600

    Task 1

---
+++ Task001/Program.cs
@@ -0,0 +1,10 @@
+﻿Console.Write("Введите первое число: ");
+int na = Convert.ToInt32(Console.ReadLine());
+Console.Write("Введите второе число: ");
+int nb = Convert.ToInt32(Console.ReadLine());
+
+if(na > nb)
+    Console.WriteLine("Первое число больше второго");
+
+if(na < nb)
+    Console.WriteLine("Второе число больше первого");
