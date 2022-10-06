1618dec5a192118231e73d8779e9601a8ca50ed5
Akmaral <axma.mail@gmail.com>
2022-10-06 18:10:58 +0600

    исправления задачи 4

--- Task004/Program.cs
+++ Task004/Program.cs
@@ -1,9 +1,8 @@
 ﻿//  Напишите программу, которая на вход принимает число (N), а на выходе показывает все чётные числа от 1 до N.
+Console.Clear();
 Console.Write("Введите число: ");
 int n = Convert.ToInt32(Console.ReadLine());
 
-
-for (int i = 1; i <= n; i++) 
-   if (i % 2 == 0)
+for (int i = 2; i <= n; i+=2) // в for указываются начальное значение, условие и шаг
     Console.Write(i + " ");
  
